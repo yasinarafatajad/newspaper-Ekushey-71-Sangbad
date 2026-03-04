a44f87c27f134570808b6b1684ca3ddf681aec08
yasinarafatajad <yasinarafatazad082@gmail.com>
2026-03-05 03:18:49 +0600

    [UI]: add reusable title bar component

--- CLIENT/src/components/Layout/CategoryBar.tsx
+++ CLIENT/src/components/Layout/CategoryBar.tsx
@@ -16,8 +16,8 @@ const categories: Category[] = [
 
 export const CategoryBar = () => {
     return (
-        <div className="bg-background-light sticky top-0 backdrop-blur-2xl z-50 border-b border-neutral-muted">
-            <div className="max-w-7xl mx-auto px-4 md:px-6 overflow-x-auto">
+        <div className="hidden lg:block bg-background-light sticky top-0 backdrop-blur-2xl z-50 border-b border-neutral-muted">
+            <div className="container overflow-x-auto">
                 <nav className="flex items-center justify-center flex-wrap whitespace-nowrap text-sm md:text-base font-medium py-2 backdrop-blur-sm">
                     {categories.map((cat, idx) => (
                         <Link
---
+++ CLIENT/src/components/Layout/TitleBar.tsx
@@ -0,0 +1,29 @@
+import { Zap } from 'lucide-react'
+import React from 'react'
+
+const headlines: string[] = [
+    'রাজধানীতে ভারী বৃষ্টির সম্ভাবনা, জনজীবনে স্বস্তি',
+    'শেয়ার বাজারে সূচকের উর্ধ্বগতি, বিনিয়োগকারীদের মুখে হাসি',
+    'টি-টোয়েন্টি বিশ্বকাপের প্রস্তুতি ম্যাচে সহজ জয় বাংলাদেশের',
+];
+
+export const TitleBar = () => {
+    return (
+        <div className="bg-neutral-subtle dark:bg-primary/5 py-2 border-b border-neutral-muted dark:border-primary/10">
+            <div className="container flex items-center overflow-hidden">
+                <div className="bg-primary text-white text-xs font-bold px-3 py-1 rounded mr-4 shrink-0 flex items-center gap-1 z-30">
+                    <Zap /> ব্রেকিং নিউজ
+                </div>
+                <div className="z-20 overflow-hidden border-l-3 py-1.5 border-primary/40">
+                    <div className="flex items-center animate-marquee lg:animate-marquee-lg">
+                        {headlines.map((title, idx) => (
+                            <p key={idx} className={`text-sm font-medium whitespace-nowrap px-2 ${idx === headlines.length - 1 ? 'border-r-0' : 'border-r-3 border-primary'}`}>
+                                {title}
+                            </p>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
