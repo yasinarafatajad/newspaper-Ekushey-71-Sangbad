2f1d4892ddcad9bf096f6ea89adcef249c1ebd60
yasinarafatajad <yasinarafatazad082@gmail.com>
2026-03-10 23:14:16 +0600

    [Page]: implement all categories and all news sections

--- CLIENT/src/app/category/page.tsx
+++ CLIENT/src/app/category/page.tsx
@@ -1,7 +1,23 @@
+import { ArticleGrid } from "@/components/ui/ArticleGrid"
+import { articles } from "@/lib/newses"
+import Link from "next/link"
 
 const Category = () => {
+
+  if (!articles)
+    return 'no article here'
+
   return (
-    <div>All Category here</div>
+    <main className="container py-8">
+      <h1 className="text-3xl font-bold mb-6 uppercase">News - based on all category</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {articles?.map(news => (
+          <Link key={news.id} href={`/news/${news.slug}`}>
+            <ArticleGrid article={news} />
+          </Link>
+        ))}
+      </div>
+    </main>
   )
 }
 
--- CLIENT/src/app/news/page.tsx
+++ CLIENT/src/app/news/page.tsx
@@ -1,4 +1,7 @@
+import { ArticleGrid } from "@/components/ui/ArticleGrid";
+import { articles } from "@/lib/newses";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Ekushey 71 Sangbad | All News",
@@ -7,7 +10,16 @@ export const metadata: Metadata = {
 
 const page = () => {
   return (
-    <div>all news here</div>
+    <main className="container py-8">
+      <h1 className="text-3xl font-bold mb-6 uppercase">All Updated News</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {articles?.map(news => (
+          <Link key={news.id} href={`/news/${news.slug}`}>
+            <ArticleGrid article={news} />
+          </Link>
+        ))}
+      </div>
+    </main>
   )
 }
 
