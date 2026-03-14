ef12ff504bcc47b82669766b4faccad6142af538
Yasin Arafat Ajad <yasinarafatazad082@gmail.com>
2026-03-15 00:20:17 +0600

    [Auth]: remove sign-up routes (#42)

--- ADMIN/src/App.tsx
+++ ADMIN/src/App.tsx
@@ -15,7 +15,7 @@ import Authors from "./pages/Authors";
 import NewsDetails from "./pages/NewsDetails";
 import AuthorDetails from "./pages/AuthorDetails";
 import Login from "./pages/auth/Login";
-import SignUp from "./pages/auth/SignUp";
+// import SignUp from "./pages/auth/SignUp";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import Verification from "./pages/auth/Verification";
 
@@ -32,7 +32,7 @@ const App = () => (
         <Routes>
           {/* Auth Routes */}
           <Route path="/login" element={<Login />} />
-          <Route path="/signUp" element={<SignUp />} />
+          {/* <Route path="/signUp" element={<SignUp />} /> */}
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/verification" element={<Verification />} />
 
