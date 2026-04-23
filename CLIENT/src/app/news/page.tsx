2d4b7b41bc334b71ec9064eca13bd9f3c3f3c1b7
Yasin Arafat Ajad <yasinarafatazad082@gmail.com>
2026-04-23 22:18:12 +0600

    [News]: sort all news in reverse order

--- CLIENT/src/app/news/page.tsx
+++ CLIENT/src/app/news/page.tsx
@@ -30,7 +30,7 @@ const page = async () => {
     <main className="container py-8">
       <h1 className="text-3xl font-bold mb-6 uppercase"> সর্বশেষ সংবাদ</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {articles?.map(news => (
+        {articles?.reverse().map(news => (
           <Link key={news._id} href={`/news/${news.slug}`}>
             <ArticleGrid article={news} />
           </Link>
